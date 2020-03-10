4bf08534183a8eb48986b4dbe071cb318f1a2d74
tylergreason <tyler.greason@protonmail.com>
2020-03-10 16:19:34 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
 // Code DigitalClicker Component Here
+// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+    constructor(){
+        super(); 
+        this.state={
+            timesClicked:0
+        }
+    }
+    // define method to run when button is clicked 
+    increaseCount = () => {
+        this.setState(previousState => {
+            return {
+                timesClicked: previousState.timesClicked+1
+            }
+
+        })
+    }
+    render(){
+        return(
+            <button
+            onClick = {this.increaseCount}
+            
+            >{this.state.timesClicked}</button>
+
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,62 @@
 // Code YouTubeDebugger Component Here
+// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+    constructor(){
+        super();
+        this.state={
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                resolution: '1080p'
+                }
+            }
+        }
+    }
+    // change bitrate 
+    changeBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings, 
+                bitrate: 12
+            }
+        })
+    }
+
+    changeResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings, 
+                video: { 
+                    ...this.state.settings.video, 
+                    resolution: '720p'
+                }
+            }
+        })
+        console.log(this.state.settings.video.resolution)
+    }
+
+
+    render(){
+        return(
+            <div>
+                <button
+                className={'bitrate'}
+                onClick={this.changeBitrate}
+                >
+                </button>
+                <button
+                className={'resolution'}
+                onClick={this.changeResolution}
+                >
+                </button>
+
+
+            </div>
+
+        )
+    }
+}
